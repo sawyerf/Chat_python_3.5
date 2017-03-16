b103b4a56b3b75ce441e2cc980079ebe0d070532
sawyerf <sawyer.flink@gmail.com>
2017-03-16 19:06:29 +0100

    Update client_chat.py

--- client_chat.py
+++ client_chat.py
@@ -1,13 +1,14 @@
 from tkinter import *
 from threading import Thread
 import socket
+import sys
 
 
 class server(Thread):
 	def __init__(self):
 		Thread.__init__(self)
-		self.pseudo="Your_name"
-		self.host="Your_ip"
+		self.pseudo="AlaeRoport"
+		self.host="localhost"
 		self.port=25565
 	
 	def run(self):
@@ -26,34 +27,37 @@ class server(Thread):
 		
 	def recevoir(self):
 		while True:
-			msg = self.main_co.recv(99999)
+			try:
+				msg = self.main_co.recv(99999)
+			except ConnectionResetError:
+				chat.insert(END, "[*]Deconnected To Server\n")
+				break
+			except ConnectionAbortedError:
+				chat.insert(END, "[*]Deconnected To Server\n")
+				break
 			msg = msg.decode()
 			chat.insert(END, msg)
 			chat.see("end")
 	
 	def recup_msg(self):
 		msg = msg_send.get()
-		msg = self.pseudo + " > " + msg + "\n"
-		self.main_co.send(msg.encode())
-		msg_send.delete(0, END)
+		if msg != '':
+			msg = self.pseudo + " > " + msg + "\n"
+			self.main_co.send(msg.encode())
+			msg_send.delete(0, END)
 		
 
 thread1 = server()
-thread1.start()
 #------------------------------GRAPHIC INTERFACE-----------------------------#
 main = Tk()
-
 chat = Text(main)
 msg_send = Entry(main, width = 90)
 send = Button(main, text="Send", command=thread1.recup_msg)
 scrollbar = Scrollbar(main, command=chat.yview, cursor="heart")
-
-
-
 #-----------------------------MENUBAR-----------------------------#
 menubar = Menu(main)
 menu1 = Menu(menubar, tearoff=0)
-menu1.add_command(label="Connect", command=main.quit)
+menu1.add_command(label="Connect", command=connection)
 menu1.add_command(label="Quitter", command=main.quit)
 menubar.add_cascade(label="Main", menu=menu1)
 main.config(menu=menubar)
@@ -63,4 +67,6 @@ chat.pack(side=TOP, padx=5, pady=1)
 msg_send.pack(side=LEFT, padx=5, pady=1)
 send.pack(side=RIGHT, padx=5, pady=1)
 
+thread1.start()
 main.mainloop()
+sys.exit(0)
