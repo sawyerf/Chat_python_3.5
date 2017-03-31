aa869aa98cbe335ba19aaa8ef30e98d83bef5ef9
sawyerf <sawyer.flink@gmail.com>
2017-03-31 14:29:08 +0200

    Update server_chat.py

--- server_chat.py
+++ server_chat.py
@@ -21,12 +21,14 @@ class soclet(Thread):
 		self.main_co.listen(-1)
 		ask_mdp = []
 		while True:
+			#--------------------ASK CONNECT--------------------------#
 			asks, wlist, rlist = select.select([self.main_co], [], [], 0)
 			for ask in asks:
 				client_nom, infos = ask.accept()
 				self.client_mdp.append(client_nom)
 				print("[*]Connected to " + str(infos[0]))
-				
+
+			#--------------------PASSWORD-----------------------------#
 			try:
 				ask_mdp, wlist, rlist = select.select(self.client_mdp, [], [], 0.05)
 			except:
@@ -43,14 +45,17 @@ class soclet(Thread):
 							mdp = mdp.decode()
 							mdp_split = mdp.split(" ")
 							if mdp_split[0] == self.mdp:
+								self.pseudo[asker_mdp] = mdp_split[1]
+								msg = "[*]" + self.pseudo[asker_mdp] + " Is Connected\n"
+								self.send_msg_all(msg.encode())
 								self.send_msg(asker_mdp, b"[*]Confirm\n")
 								self.send_msg(asker_mdp, b"[*]Welcome To The Server\n")
-								self.pseudo[str(asker_mdp)] = mdp_split[1]
 								self.client_co.append(asker_mdp)
 								self.client_mdp.remove(asker_mdp)
 							else:
 								self.send_msg(asker_mdp, b"[*]Try Again\n")
-					
+
+			#------------------------MESSAGE--------------------------#
 			try:
 				atts, wlist, rlist = select.select(self.client_co, [], [], 0.05)
 			except select.error:
@@ -63,22 +68,29 @@ class soclet(Thread):
 					except ConnectionResetError:
 						pass
 					else:
-						msg = self.pseudo[str(att)] + " > " + msg
-						i = 0
-						for co in self.client_co:
-							try:
-								co.send(msg.encode())
-							except ConnectionResetError:
-								del self.client_co[i]
-							except:
-								pass
-							i = i + 1
+						msg_split = msg.split(" ")
+						if msg_split[0]=="/pseudo" :
+							self.pseudo[att] = msg_split[1]
+						else:
+							msg = self.pseudo[att] + " > " + msg + "\n"
+							self.send_msg_all(msg.encode())
 
 	def send_msg(self, recver, msg_asend):
 		try:
 			recver.send(msg_asend)
 		except:
 			pass
+
+	def send_msg_all(self, msg_all):
+		i = 0
+		for co in self.client_co:
+			try:
+				co.send(msg_all)
+			except ConnectionResetError:
+				del self.client_co[i]
+			except:
+				pass
+			i = i + 1
 
 
 print("[*]Server Start\n")
