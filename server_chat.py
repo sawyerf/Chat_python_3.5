b36d3c2318bf97eab8b05a1886381a63ba6a2b37
sawyerf <sawyer.flink@gmail.com>
2017-04-07 23:44:10 +0200

    Update server_chat.py

--- server_chat.py
+++ server_chat.py
@@ -1,146 +1,168 @@
 #Version 2.2.3
+version = "2.2.3"
 
-from tkinter import *
-from threading import Thread
+import select
 import socket
-import sys
-import hashlib
+from threading import Thread
 
-
-class Server(Thread):
+class soclet(Thread):
 	def __init__(self):
 		Thread.__init__(self)
-		self.pseudo=""
-		self.host=""
-		self.port=25565
-		self.confirm=False
-		self.one_error=True
-		self.condition=True	
-
-	def run(self):
-		while self.condition:
-			try:
-				self.main_co = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-				self.main_co.connect((self.host, self.port))
-				self.main_co.settimeout(0.5)
-			except ConnectionRefusedError:
-				if self.one_error == True:
-					self.chat_insert("[*]No Server\n")
-					self.one_error=False
-				pass
-			except:
-				if self.one_error == True:
-					self.chat_insert("[*]Error Server\n")
-					self.one_error = False
-				break
-			else:
-				self.chat_insert("[*]Connected To Server\n")
-				self.recevoir()
-				break
+		self.host = ""
+		self.port = 25565
+		self.mdp = "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8"		#password is "password" in SHA-1
+		self.mdp_modo = "d24fa7248f9f2a4c744f1adc1ff59f3c4002cb16"	#password is "passwordmodo" in SHA-1
+		self.client_mdp = []
+		self.client_co = []
+		self.pseudo = dict()
+		self.rang = dict()
 		
-	def recevoir(self):
-		while self.condition:
-			msg=""
+	def run(self):
+		self.main_co = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.main_co.bind((self.host, self.port))
+		self.main_co.listen(-1)
+		ask_mdp = []
+		while True:
+			#--------------------ASK CONNECT--------------------------#
+			asks, wlist, rlist = select.select([self.main_co], [], [], 0)
+			for ask in asks:
+				client_nom, infos = ask.accept()
+				self.client_mdp.append(client_nom)
+				print("[*]Connected to " + str(infos[0]))
+
+			#--------------------PASSWORD-----------------------------#
 			try:
-				msg = self.main_co.recv(1024)
-			except ConnectionResetError:
-				self.chat_insert("[*]Deconnected To Server\n")
-				break
-			except ConnectionAbortedError:
-				self.chat_insert("[*]Deconnected To Server\n")
-				break
-			except BrokenPipeError:
-				self.chat_insert("[*]Deconnected To Server\n")
-				break
+				ask_mdp, wlist, rlist = select.select(self.client_mdp, [], [], 0.05)
 			except:
 				pass
 			else:
-				if msg != "":
-					msg = msg.decode()
-					if "[*]Confirm\n" in msg:
-						self.confirm = True
-					self.chat_insert(msg)
-					interface.chat.see("end")
-		self.main_co.send(b"/quit")
-		self.main_co.close()
+				for asker_mdp in ask_mdp:
+					mdp = ""
+					try:
+						mdp = asker_mdp.recv(1024)
+					except:
+						pass
+					else:
+						if mdp != "":
+							mdp = mdp.decode()
+							mdp_split = mdp.split(" ")
+							if mdp_split[0]==self.mdp or mdp_split[1]==self.mdp_modo:
+								nombre = len(mdp_split[1])
+								while nombre<10:
+									mdp_split[1] += " "
+									nombre = len(mdp_split[1])
+								self.pseudo[asker_mdp] = mdp_split[1]
+								#------------CHOOSE-RANG---------------#
+								if mdp_split[0]==self.mdp:
+									self.rang[asker_mdp] = "normal"
+								elif mdp_split[0]==self.mdp_modo:
+									self.rang[asker_mdp] = "modo"
+								#----------END-CHOOSE-RANG------------#
+								msg = "[*]" + self.pseudo[asker_mdp] + " Is Connected\n"
+								self.send_msg_all(msg.encode())
+								self.send_msg(asker_mdp, b"[*]Confirm\n")
+								self.send_msg(asker_mdp, b"[*]Welcome To The Server\n")
+								self.client_co.append(asker_mdp)
+								self.client_mdp.remove(asker_mdp)
+							else:
+								self.send_msg(asker_mdp, b"[*]Try Again\n")
 
-
-	def recup_msg(self, enter=""):
-		msg = interface.msg_send.get()
-		if msg != '':
-			if self.confirm == True:
-				msg = msg
-			else:
-				msg = hashlib.sha1(msg.encode()).hexdigest()
-				msg = msg + " " + self.pseudo
+			#------------------------MESSAGE--------------------------#
+			try:
+				atts, wlist, rlist = select.select(self.client_co, [], [], 0.05)
+			except select.error:
 				pass
-			self.main_co.send(msg.encode())
-			interface.msg_send.delete(0, END)
-	
-	def chat_insert(self, msg):
-		interface.chat.configure(state=NORMAL)
-		interface.chat.insert(END, msg)
-		interface.chat.tag_config("alae", foreground="#FF8000")
-		interface.chat.configure(state=DISABLED)
-	
+			else:
+				for att in atts:
+					msg=""
+					try:
+						msg = att.recv(1024)
+						msg = msg.decode()
+					except ConnectionResetError:
+						pass
+					except:
+						pass
+					else:
+						if msg != "":
+							msg_split = msg.split(" ")
+							#--------------COMMANDS---------------#
+							if msg_split[0]=="/help":
+								msg = """[*]The Commands is :
+   /help 				For known all the commands
+   /nick + YourName				For change your nick name
+   /version 				For know the version of the server
+   /who 				For know who is connect on the server
+   /kick + Bitch 				For kick a fucking bitch
+   /quit 				For quit the server where you are\n"""
+								att.send(msg.encode())
+							elif msg_split[0]=="/nick":
+								try:
+									if len(msg_split[1])<=10:
+										nombre = len(msg_split[1])
+										while nombre<10:
+											msg_split[1] += " "
+											nombre = len(msg_split[1])
+											print(nombre)
+										self.pseudo[att] = msg_split[1]
+									else:
+										att.send(b"[*]Your New Nickname Must Do Less 10 Caractere\n")
+								except:
+									att.send(b"[*]Your New Nickname Dosn't Work\n")
+							elif msg_split[0]=="/version":
+								msg = "[*]Version " + version + "\n"
+								att.send(msg.encode())
+							elif msg_split[0] == "/who":
+								msg = ""
+								for co in self.client_co:
+									msg = msg + self.pseudo[co] + "/ "
+								msg = "[*]Who > " + msg + "\n"
+								att.send(msg.encode())
+							elif msg_split[0]=="/kick":
+								if self.rang[att] == "modo":
+									for ps in self.pseudo:
+										try:
+											if self.pseudo[ps]==msg_split[1]:
+												msg = "[*]" + self.pseudo[ps] + " Is Kick\n"
+												self.send_msg_all(msg.encode())
+												self.client_co.remove(ps)
+												del self.pseudo[ps]
+												break
+										except:
+											att.send(b"[*]:O\n")
+								else:
+									att.send(b"[*]Your Are Not Modo\n")
+							elif msg_split[0]=="/quit":
+								msg = "[*]" + self.pseudo[att] + " Is Disconect\n"
+								self.send_msg_all(msg.encode())
+								del self.pseudo[att]
+								self.client_co.remove(att)
+								print(msg)
+							else:
+								msg = self.pseudo[att] + " > " + msg + "\n"
+								self.send_msg_all(msg.encode())
 
-class Interface(Thread):
-	def __init__(self):
-		Thread.__init__(self)
+	def send_msg(self, recver, msg_asend):
+		try:
+			recver.send(msg_asend)
+		except:
+			pass
 
-	def run(self):
-		#----------------------GRAPHIC INTERFACE--------------------------#
-		self.main = Tk()
-		#self.main.geometry("750x500")
-		self.chat = Text(self.main, state=NORMAL)
-		self.msg_send = Entry(self.main, width=92)
-		self.scrollbar = Scrollbar(self.main, command=self.chat.yview)
-		self.msg_send.bind("<Return>", server.recup_msg)
-		#-----------------------------MENUBAR-----------------------------#
-		self.menubar = Menu(self.main)
-		self.menu1 = Menu(self.menubar, tearoff=0)
-		self.menu1.add_command(label="Connect", command=self.main.quit)
-		self.menu1.add_command(label="Quitter", command=self.main.quit)
-		self.menubar.add_cascade(label="Main", menu=self.menu1)
-		self.main.config(menu=self.menubar)
-		#-----------------------------PACK-------------------------------#
-		self.msg_send.pack(side=BOTTOM, padx=5, pady=2, fill=X)
-		self.scrollbar.pack(side=RIGHT,fill=Y)
-		self.chat.pack(side=TOP, padx=5, pady=2, fill=BOTH, expand=1)
-		self.chat.configure(state=DISABLED)
+	def send_msg_all(self, msg_all):
+		i = 0
+		for co in self.client_co:
+			try:
+				co.send(msg_all)
+			except ConnectionResetError:
+				msg = "[*]" + self.pseudo[self.client_co[i]] + " Is Disconect"
+				del self.client_co[i]
+				self.send_msg_all(mdg.encode())
+				print(msg)
+			except:
+				pass
+			i = i + 1
 
-		server.start()
-		self.main.mainloop()
-		server.condition=False
-		sys.exit(0)
 
-def Profil():
-	#-----------------GRAPHIC INTERFACE---------------------#
-	main_profil = Tk()
-	ip_label = Label(main_profil, text="Ip Server:")
-	ip_entry = Entry(main_profil, width=34)
-	pseudo_label = Label(main_profil, text="Pseudo:")
-	pseudo_entry = Entry(main_profil, width=34)
-	valid_button = Button(main_profil, text="Confirm", command=lambda: confirm_profil("", main_profil, ip_entry.get(), pseudo_entry.get()), width=11)
-	cancel_button = Button(main_profil, text="Cancel", command=main_profil.destroy, width=11)
-	main_profil.bind("<Return>", lambda eff: confirm_profil(eff, main_profil, ip_entry.get(), pseudo_entry.get()))
-	#--------------------------PACK-------------------------#
-	ip_label.pack(side=TOP)
-	ip_entry.pack(side=TOP, padx=15)
-	pseudo_label.pack(side=TOP)
-	pseudo_entry.pack(side=TOP, padx=15)
-	valid_button.pack(side=RIGHT, padx=15, pady=5)
-	cancel_button.pack(side=LEFT, padx=15, pady=5)
-	main_profil.mainloop()
-
-def confirm_profil(enter, main_profil, ip, pseudo):
-	server.host = ip
-	server.pseudo = pseudo
-	if server.host!="" and server.pseudo!="" and len(pseudo)<=10:
-		main_profil.destroy()
-
-server = Server()
-interface = Interface()
-
-Profil()
-interface.start()
+print("[*]Server Start\n")
+print("Your Ip Is " + socket.gethostbyname(socket.gethostname()))
+thread1 = soclet()
+thread1.start()
